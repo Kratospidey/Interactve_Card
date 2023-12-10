3bd896206fa1469ae0fa7729244293ed27fb9120
Kratospidey <devtools@kratospidey.com>
2023-12-11 01:38:22 +0530

    add card position and 3d layout

---
+++ script.js
@@ -0,0 +1,15 @@
+VanillaTilt.init(document.querySelector("#front-card"), {
+	max: 25,
+	speed: 400,
+	glare: true,
+	"max-glare": 0.5,
+});
+
+VanillaTilt.init(document.querySelector("#back-card"), {
+	max: 25,
+	speed: 400,
+	glare: true,
+	"max-glare": 0.5,
+});
+
+
